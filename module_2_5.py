1d9afe08244690d2c9500cc9406b16d17a00fba5
EniaLui <eniana@mail.ru>
2024-08-30 12:42:21 +0300

    Add files via upload

---
+++ module_2_5.py
@@ -0,0 +1,15 @@
+def get_matrix (n, m, value):
+    matrix = []
+    matrixdop = []
+    for i in (0, int(n)):
+        matrixdop.append(value)
+        for j in range(0, int(m)):
+            matrix.append(matrixdop)
+    print(matrix)
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
